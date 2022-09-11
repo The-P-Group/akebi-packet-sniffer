7bf201d3138563a57a2a3ee435a8bfd77dd29cb4
Dang Hoang Phuc <dhp371.work@gmail.com>
2022-09-11 13:10:55 +0700

    feat: ignroe packet

--- src/managers/packetProcess.ts
+++ src/managers/packetProcess.ts
@@ -2,16 +2,19 @@
 import { Packet } from './packet';
 import { PacketDirection, PacketType } from '../types/enum';
 import { AkebiWebSocket } from './webSocket';
+import config from '../constants/config.json';
+import chalk from 'chalk';
 
 export class PacketProcess {
 	static process(data: Buffer) {
 		try {
 			const packet = new Packet(data);
 
-			if (packet.packetType == PacketType.PACKET_DATA || packet.packetType == PacketType.MODIFY_DATA) {
-				// console.log(packet.contentData);
-				console.log(packet.direction === PacketDirection.SEND ? 'SEND: ' : 'RECV: ', packet.packetName);
-				AkebiWebSocket.addToQueue(packet);
+			if (packet.packetType === PacketType.PACKET_DATA || packet.packetType === PacketType.MODIFY_DATA) {
+				if (!config.ignorePacket.includes(packet.packetName)) {
+					console.log(packet.direction === PacketDirection.SEND ? chalk.yellow('SEND: ' + packet.packetName) : chalk.magenta('RECV: ' + packet.packetName));
+					AkebiWebSocket.addToQueue(packet);
+				}
 			}
 		} catch (e: any) {
 			console.error(e?.message);
